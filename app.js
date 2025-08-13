b4648d820a3fdbfcfca97535bcbba0acc6de7389
Paulo Toledo <paulojrtoledo@gmail.com>
2025-08-12 21:31:15 -0300

    Criando função de adicionar

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+res = document.querySelector('#resultado');
+
+function adicionarAmigo() {
+
+    let amigoAdd = document.querySelector('#amigo');
+
+    if (amigoAdd.value.trim() == '') {
+        alert('Por favor, insira um nome')
+    }
+
+    amigos.push(amigoAdd.value);
+    res.innerHTML = amigos.join('<br>');
+    amigoAdd.value = ''
+}
+
